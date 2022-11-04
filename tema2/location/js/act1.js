24dd734d7bca3b2ceec7bd8644d0ae1756013a42
lcasort <laucasort.notes@gmail.com>
2022-11-04 20:21:57 +0100

    Location.

---
+++ tema2/location/js/act1.js
@@ -0,0 +1,8 @@
+/*
+Actividad 1. Propiedades de la URL.
+Implementa una aplicación que muestre la siguiente información sobre la URL de la página:
+ La URL completa de la página. Propiedad href.
+ El nombre de dominio. Propiedad hostname.
+ La ruta y el nombre del fichero de la página. Propiedad pathname.
+ El protocolo de comunicación utilizado. Propiedad protocol.
+*/
