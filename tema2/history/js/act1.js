8f4bb98acae77373b29eeecc5e1d3016ce2124bb
lcasort <laucasort.notes@gmail.com>
2022-11-07 16:42:29 +0100

    Navigator and history activities done.

---
+++ tema2/history/js/act1.js
@@ -0,0 +1,15 @@
+function back()
+{
+    history.back();
+}
+
+function forward()
+{
+    history.forward();
+}
+
+function go()
+{
+    // Hace un reload.
+    history.go(0);
+}
--- tema2/navigator/js/act1.js
+++ tema2/navigator/js/act1.js
@@ -7,3 +7,43 @@ userAgent.
  El sistema operativo sobre el que corre el navegador. Propiedad platform.
  El lenguaje del navegador. Propiedad language.
 */
+function getHeader()
+{
+    const node = document.createElement('p');
+    const userAgent = navigator.userAgent;
+    const textNode = document.createTextNode('navigator.userAgent: ' + userAgent);
+    node.appendChild(textNode);
+    document.querySelector('.act1').appendChild(node);
+}
+
+function getCookiesEnabled()
+{
+    const node = document.createElement('p');
+    const boolCookies = navigator.cookieEnabled;
+    const textNode = document.createTextNode('navigator.cookieEnabled: ' + boolCookies);
+    node.appendChild(textNode);
+    document.querySelector('.act1').appendChild(node);
+}
+
+function getOperatingSystem()
+{
+    const node = document.createElement('p');
+    const opSys = navigator.platform;
+    const textNode = document.createTextNode('navigator.platform: ' + opSys);
+    node.appendChild(textNode);
+    document.querySelector('.act1').appendChild(node);
+}
+
+function getNavigatorLanguage()
+{
+    const node = document.createElement('p');
+    const lan = navigator.language;
+    const textNode = document.createTextNode('navigator.language: ' + lan);
+    node.appendChild(textNode);
+    document.querySelector('.act1').appendChild(node);
+}
+
+getHeader();
+getCookiesEnabled();
+getOperatingSystem();
+getNavigatorLanguage();
