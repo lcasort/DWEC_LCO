60fe9b526f48874c890ba99756c99654bf810bb1
lcasort <laucasort.notes@gmail.com>
2022-11-16 16:50:50 +0100

    Actividad array clase.

---
+++ tema2/arrays/js/act1.js
@@ -0,0 +1,35 @@
+/**
+ * Función para filtrar los elementos de un array por la condición dada.
+ * @param {array} array 
+ * @param {function} callback 
+ */
+function filter(array, callback)
+{
+    let filteredArray = [];
+
+    array.forEach(x => {
+        if(callback(x)) {
+            filteredArray.push(x);
+        }
+    });
+
+    return filteredArray;
+}
+
+/**
+ * Función que devuelve true si el número pasado por parámetro es mayor o igual
+ * que 10 y false en caso contrario.
+ * @param {number} num 
+ */
+function isGreaterEqualThan10(num)
+{
+    return num >= 10;
+}
+
+
+
+// CONSTANTS
+const a = [10,30,8,7,50,2];
+
+// MAIN
+console.log(filter(a,isGreaterEqualThan10));
