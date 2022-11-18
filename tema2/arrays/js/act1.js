3327edeb407c5ef5c1896e1012d6bae82df2dbac
lcasort <laucasort.notes@gmail.com>
2022-11-18 17:24:18 +0100

    isEven callback function.

--- tema2/arrays/js/act1.js
+++ tema2/arrays/js/act1.js
@@ -25,7 +25,7 @@ function find(array, callback)
     let res = null;
 
     for(let i=0; i<array.length && res===null; i++) {
-        if(isGreaterEqualThan10(array[i])) {
+        if(callback(array[i])) {
             res = array[i];
         }
     }
@@ -43,11 +43,24 @@ function isGreaterEqualThan10(num)
     return num >= 10;
 }
 
+/**
+ * Función que devuelve true si el número pasado por parámetro es par y false
+ * en caso contrario.
+ * @param {number} num 
+ * @returns 
+ */
+function isEven(num)
+{
+    return num%2 == 0;
+}
+
 
 
 // CONSTANTS
 const a = [10,30,8,7,50,2];
+const elems = [30,2,3,8,24];
 
 // MAIN
 console.log(filter(a,isGreaterEqualThan10));
 console.log(find(a,isGreaterEqualThan10));
+console.log(filter(elems,isEven));
