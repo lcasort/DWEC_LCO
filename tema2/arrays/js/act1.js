7393e01b0514ed95439ff5a7f0020ac8b340d12c
lcasort <laucasort.notes@gmail.com>
2022-11-18 18:35:21 +0100

    First repeated element.

--- tema2/arrays/js/act1.js
+++ tema2/arrays/js/act1.js
@@ -34,6 +34,26 @@ function find(array, callback)
 }
 
 /**
+ * Función para devolver el primer elemento que cumpla la condición dada.
+ * @param {function} callback 
+ * @param {number} index 
+ * @param {array} array 
+ * @returns 
+ */
+function find(array, callback)
+{
+    let res = null;
+
+    for(let i=0; i<array.length && res===null; i++) {
+        if(callback(array[i], i+1, array)) {
+            res = array[i];
+        }
+    }
+
+    return res;
+}
+
+/**
  * Función que devuelve true si el número pasado por parámetro es mayor o igual
  * que 10 y false en caso contrario.
  * @param {number} num 
@@ -54,18 +74,38 @@ function isEven(num)
     return num%2 === 0;
 }
 
+/**
+ * Función que devuelve el primer número repetido del array.
+ * @param {number} elem 
+ * @param {number} index 
+ * @param {array} array 
+ * @returns 
+ */
+function firstRepeated(elem, index, array)
+{
+    let res = false;
+
+    for(let i=index; i<array.length && res===false; i++) {
+        if(elem === array[i]) {
+            res = true;
+        }
+    }
+
+    return res;
+}
+
+
 
 
 // CONSTANTS
 const a = [10,30,8,7,50,2];
 const elems = [30,2,3,8,24];
+const els = [3,6,9,7,7,4,9];
 
 // MAIN
 console.log(filter(a,isGreaterEqualThan10));
 console.log(find(a,isGreaterEqualThan10));
 console.log(filter(elems,isEven));
-console.log(filter(elems, function (num)
-{
-    return num%2 === 0;
-}));
+console.log(filter(elems, function (num) { return num%2 === 0; }));
 console.log(filter(elems, num => num%2 === 0));
+console.log(find(els,firstRepeated));
