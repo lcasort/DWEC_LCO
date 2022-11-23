4bf55b2bbe5efa8ddf392c8147fc701d3be4af8f
lcasort <laucasort.notes@gmail.com>
2022-11-23 17:47:55 +0100

    A

---
+++ tema3/examples/js/ex1.js
@@ -0,0 +1,12 @@
+let suma = (a,b) => a+b; 
+console.log(suma(2,3));
+
+let sinParams = () => {console.log('Hola.'); return 'Adios.';}
+console.log(sinParams());
+
+// (p1,p2,...) => sentencia
+// (p1,p2,...) => {sentencias}
+// () => sentencia
+// () => {sentencias}
+// p => sentencia
+// p => {sentencias}
