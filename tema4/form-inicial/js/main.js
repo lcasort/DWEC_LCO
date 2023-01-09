4866421a5873e28d624989d87c5412ede716f3a7
lcasort <laucasort.notes@gmail.com>
2023-01-09 20:13:16 +0100

    a

--- tema4/form-inicial/js/main.js
+++ tema4/form-inicial/js/main.js
@@ -5,3 +5,18 @@ let formTag = document.getElementsByTagName('form');
 console.log(formTag);
 
 console.log(formId === formTag[0]); // True
+
+////////////////////////////////////////////////////////////////////////////////
+
+let container = document.getElementById('container');
+let form = container.getElementsByTagName('form');
+console.log(form[0]);
+
+////////////////////////////////////////////////////////////////////////////////
+
+let formNameForms = document.forms['formulario'];
+console.log(formNameForms);
+
+////////////////////////////////////////////////////////////////////////////////
+
+console.log(formNameForms.action);
