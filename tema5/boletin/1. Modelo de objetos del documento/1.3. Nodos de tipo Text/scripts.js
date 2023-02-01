57c790fb85b68bda59706584ecf870f5561ce222
lcasort <laucasort.notes@gmail.com>
2023-02-01 17:53:42 +0100

    a

--- tema5/boletin/1. Modelo de objetos del documento/1.3. Nodos de tipo Text/scripts.js
+++ tema5/boletin/1. Modelo de objetos del documento/1.3. Nodos de tipo Text/scripts.js
@@ -8,7 +8,10 @@
 // pero comprueba que no haya espacios en blanco entre el elemento div y p.   //
 // Puede tomar el primer hijo de div como un espacio en blanco.               //
 ////////////////////////////////////////////////////////////////////////////////
-
+const divCallout2 = document.querySelector('#callout2 p');
+console.log(divCallout2.innerHTML);
+console.log(divCallout2.textContent);
+console.log(divCallout2.nodeValue);
 
 ////////////////////////////////// APARTADO B //////////////////////////////////
 // Modifica el contenido del primer párrafo del div callout2 para añadir el   //
