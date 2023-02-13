ed4abed97fb5fdb526df60d4865951f024569401
lcasort <laucasort.notes@gmail.com>
2023-02-13 18:35:23 +0100

    Apartado 1 hecho.

--- tema5/boletin/3. Prácticas propuestas/7/Tienda online LocalStorage-20230210T172810Z-001/Tienda online LocalStorage/js/app.js
+++ tema5/boletin/3. Prácticas propuestas/7/Tienda online LocalStorage-20230210T172810Z-001/Tienda online LocalStorage/js/app.js
@@ -1,2 +1,54 @@
 // Importamos la base de datos
 import {productos} from "../db/db.js";
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// MAIN /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function iniciarListeners()
+{
+    window.addEventListener("load", cargarProductos);   
+}
+
+iniciarListeners();
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//                                AUX. METHODS                                //
+////////////////////////////////////////////////////////////////////////////////
+
+function cargarProductos()
+{
+    let div = document.querySelector("#products-container");
+
+    for (const p of productos) {
+        div.innerHTML += crearHTMLProducto(p);
+    }
+}
+
+function crearHTMLProducto(p)
+{
+    const{id,nombre,categoria,imagen,precio,vendedor,stock} = p;
+    return `
+    <article id="${id}" class="location-listing" data-categoria="${categoria}">
+        <div class="location-image">
+            <a href="#">
+                <img src="${imagen}" alt="${nombre}">
+            </a>
+        </div>
+        <div class="data">
+            <h4>${nombre}</h4>
+            <p class="price">${precio}€</p>
+            <p>Vendido por <strong>${vendedor}</strong></p>
+            <p>Quedan ${stock} unidades</p>
+            <div class="button-container">
+                <a class="button add" href="#" target="_blank">Añadir al carrito</a>
+            </div>
+        </div>
+    </article>`;
+}
+
+////////////////////////////////////////////////////////////////////////////////
